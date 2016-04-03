4659834c56e577a93bfa0bc0bf6d2ad9ce6800ec
Roben Kleene <robenk@gmail.com>
2016-04-03 14:33:44 -0400

    Remove JavaScript

--- examples/js/animation.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-  var element = document.getElementsByClassName("window")[0];
-  element.classList.remove("window-frame-1");
-  element.classList.add("window-frame-2");
-};
